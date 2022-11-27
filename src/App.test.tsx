3d18c08c7fab2b84a39af11df81e46d2ffc25c23
Florian Aumeier <hey@flo.fish>
2022-11-27 14:04:46 +0100

    add coverage thresholds

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,13 @@
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import App from './App'
 
-it('clicking counter increases count', () => {
+it('clicking counter increases count', async () => {
     render(<App />)
 
     expect(screen.getByRole('button')).toHaveTextContent('count is 0')
+
+    fireEvent.click(screen.getByRole('button'))
+
+    await waitFor(() => screen.getByRole('button'))
+    expect(screen.getByRole('button')).toHaveTextContent('count is 1')
 })
